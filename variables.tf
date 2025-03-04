de7b76c0f380778e9a88b3b78515cb6f613acf4a
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-03-04 15:46:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,18 @@
 variable "aws_instance_type" {
   description = "Instance type"
   type        = string
+  validation {
+    condition     = var.aws_instance_type == "t3.micro" || var.aws_instance_type == "t3.medium"
+    error_message = "Only t3.micro and t3.medium are allowed."
+  }
 }
 
 variable "root_volume_size" {
-  type = number
+  type    = number
   default = 20                           
+}
+
+variable "root_volume_type" {
+  type    = string
+  default = "gp2"                       
 }
