2b8e2b36ad8af932ce0c089a0fb7b3a0becfa0bd
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-03-04 15:21:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,8 @@
-variable "aws_instance_type"{
- description = "what is your instance type "
- type = string
+variable "aws_instance_type" {
+  description = "Instance type"
+  type        = string
 }
 
-variable "root_volume_size"
-{
+variable "root_volume_size" {
   type = number
 }
